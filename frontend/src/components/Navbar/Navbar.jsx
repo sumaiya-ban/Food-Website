0fc9d2a43b14753ce4afcc33f852c83ebd55d24e
sumaiya <sumaiyayesmen556@gmail.com>
2025-11-21 21:48:29 +0600

    Add cart

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,8 @@ const Navbar = ({setShowLogin}) => {
 
   return (
     <div className='navbar'>
-        <img src={assets.logo} className="logo" />
+        <Link to='/'> <img src={assets.logo} className="logo" /></Link>
+       
         <ul className="navbar-menu">
             <Link to='/' onClick={()=>setMenu("home")}  className={menu==="home"?"active":""}>home</Link>
             <a href='#explore-menu' onClick={()=>setMenu("menu")}  className={menu==="menu"?"active":""}>menu</a>
@@ -17,7 +18,8 @@ const Navbar = ({setShowLogin}) => {
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
             <div className="navbar-serach-icon">
-                <img src={assets.basket_icon} alt="" />
+                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+                
                 <div className="dot"></div>
             </div>
             <button onClick={()=>{setShowLogin(true)}}>sign in</button>
